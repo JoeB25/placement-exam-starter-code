082a352a0fe28f0875962856d66dbd7375c71dc1
JoeB25 <herojoe89@gmail.com>
2021-08-30 20:27:22 -0500

    Final exam commit

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,52 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Joe Baltazar" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+var qty-gb = document.getElementById('qty-gb')
+var add-gb = document.getElementById('add-gb')
+var minus-gb = document.getElementById('minus-gb')
+var qty-cc = document.getElementById('qty-cc')
+var add-cc = document.getElementById('add-cc')
+var minus-cc = document.getElementById('minus-cc')
+var qty-sugar = document.getElementById('qty-sugar')
+var add-sugar = document.getElementById('add-sugar')
+var minus-sugar = document.getElementById('minus-sugar')
+
+
 
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+document.getElementById('credit').textContent = `Created by ${joebaltazar}`
+
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
+
+
+
+document.getElementById('add-cc').addEventListener('click', function() {
+
+
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+
+
+   
+    })
+
+
+    
     // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+    console.log(qty-gb);
+    console.log(qty-cc)
+    console.log(gty-sugar)
 
     // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
+   
+
 
 // TODO: Hook up event listeners for the rest of the buttons
